d26b1e34d80793d5413afcd5689b3b9c4cd04ae1
Richard <richard.yang.tong@gmail.com>
2021-04-08 10:00:53 -0400

    Added a third parameter to function call to see the error in IDE.

--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ const add = (n1: number, n2: number) => {
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
 
-const result = add(number1, number2);
+const result = add(number1, number2, printResult);
 console.log(result);

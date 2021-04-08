8342b5f61fc7a9de2012b197015b3843ca21a4fc
Richard <richard.yang.tong@gmail.com>
2021-04-08 10:59:11 -0400

    Added a fourth parameter to function call and check error in IDE.

--- app.ts
+++ app.ts
@@ -9,5 +9,6 @@ const add = (n1: number, n2: number, showResult: boolean) => {
 const number1 = 5;
 const number2 = 2.8;
 const printResult = true;
+const resultPhrase = 'Result is: ';
 
-add(number1, number2, printResult);
+add(number1, number2, printResult, resultPhrase);

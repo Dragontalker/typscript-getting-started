c6d64625cf23468422f3de33d20b2760cdec9599
Richard <richard.yang.tong@gmail.com>
2021-04-08 14:29:00 -0400

    Added a new typescript code snippet.

---
+++ app.ts
@@ -0,0 +1,8 @@
+const combine = (input1: number, input2: number) => {
+    const result = input1 + input2;
+    return result;
+};
+
+const combineAges = combine(30, 26);
+
+console.log(combineAges);
